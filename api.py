1caa42ac9fb6c5693a939434a221b3b5c448cb92
itzhak-ga <itzhak.guhafi@aquant.ai>
2024-11-28 16:23:54 +0200

    api file name is changed

--- api.py
+++ api.py
@@ -34,4 +34,4 @@ def create_customer(new_customer: CustomerInputs, request: Request,
 app.include_router(router)
 
 if __name__ == '__main__':
-    uvicorn.run('main:app', host="0.0.0.0", port=8000, reload=True)
+    uvicorn.run('api:app', host="0.0.0.0", port=8000, reload=True)
